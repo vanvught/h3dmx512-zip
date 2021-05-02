4c61b308e57985122df8a908b5360aac4a2baf41
Arjan van Vught <arjan.van.vught@gmail.com>
2021-05-02 12:05:51 +0200

    Now checking for reboot

--- do-tftp.sh
+++ do-tftp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo '!tftp#1' | nc -u -p 10501 -w 1 $1 10501
 echo '?tftp#' | nc -u -p 10501 -w 1 $1 10501
@@ -25,5 +25,15 @@ fi
 
 echo '!tftp#0' | nc -u -p 10501 -w 1 $1 10501
 echo '?tftp#' | nc -u -p 10501 -w 1 $1 10501
-
+echo -e "\e[34mRebooting...\e[0m"
 echo '?reboot##' | nc -u -p 10501 -w 1 $1 10501
+
+ON_LINE=$(echo '?list#' | nc -u -p 10501 -w 1 $1 10501) || true
+
+while  [ "$ON_LINE" == "" ]
+ do
+  ON_LINE=$(echo '?list#' | nc -u -p 10501 -w 1 $1 10501)  || true
+done
+
+echo -e "\e[36m[$ON_LINE]\e[0m"
+echo -e "\e[36m$(echo '?version#' | nc -u -p 10501 -w 1 $1 10501)\e[0m"
